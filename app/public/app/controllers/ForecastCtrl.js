2df309e57b8691f3c01a01592491b03666d878d4
Momchil Anachkov <momchil.anachkov@gmail.com>
2017-01-14 18:32:52 +0200

    Fixed some src to ng-src and a ForecastCtrl error

--- app/public/app/controllers/ForecastCtrl.js
+++ app/public/app/controllers/ForecastCtrl.js
@@ -7,35 +7,35 @@
      */
     if(!$scope.model.forecast){
       $state.go('main.search');
-    }
+    } else {
+      $scope.viewType = (($state.current.name === 'main.forecast.today') ? 'today' : 'future');
     
-    $scope.viewType = (($state.current.name === 'main.forecast.today') ? 'today' : 'future');
-    
-    /**
-     * Transforms the data to a format suitable for ng-repeat:
-     */
-    $scope.forecast = (function(data) {
-      var result = [];
-      var days = data.forecast.forecastday;
-      var dateTime;
-      var item;
-      var i, j;
-      
-      for (i = 0; i < days.length; i++) {
-        for(j = 0; j < days[i].hour.length; j++) {
-          item = days[i].hour[j];
-          dateTime = item.time.split(' ');
-          result.push({
-            date: dateTime[0],
-            time: dateTime[1],
-            temp: item.temp_c,
-            iconUrl: item.condition.icon,
-            condition: item.condition.text
-          });
+      /**
+       * Transforms the data to a format suitable for ng-repeat:
+       */
+      $scope.forecast = (function(data) {
+        var result = [];
+        var days = data.forecast.forecastday;
+        var dateTime;
+        var item;
+        var i, j;
+        
+        for (i = 0; i < days.length; i++) {
+          for(j = 0; j < days[i].hour.length; j++) {
+            item = days[i].hour[j];
+            dateTime = item.time.split(' ');
+            result.push({
+              date: dateTime[0],
+              time: dateTime[1],
+              temp: item.temp_c,
+              iconUrl: item.condition.icon,
+              condition: item.condition.text
+            });
+          }
         }
-      }
-      
-      return result;
-    }($scope.model.forecast));
+        
+        return result;
+      }($scope.model.forecast));
+    }
   }]);
 }());
